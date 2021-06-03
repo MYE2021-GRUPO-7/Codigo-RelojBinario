a0a8ad66517739556a5ed0894c7c6ca3fb3d4c42
Gabriela Hernández <84205288+GabrielaH16@users.noreply.github.com>
2021-06-03 11:04:51 -0600

    Código completo

--- reloj.ino
+++ reloj.ino
@@ -2,12 +2,29 @@ int ledPinsSec[] = {2, 3, 4, 5, 6, 7};
 int ledPinsMin[] = {8, 9, 10, 11, 12, 13};
 int ledPinsHr[] = {14, 15, 16, 17, 18, 19};
 int secSize =sizeof(ledPinsSec) / sizeof(int);
-int minSize = sizeof(ledPinsMin) / sizeof(int);
-int hourSize = sizeof(ledPinsHr) / sizeof(int);
+  int minSize = sizeof(ledPinsMin) / sizeof(int);
+  int hourSize = sizeof(ledPinsHr) / sizeof(int);
 int seconds = 30;
 int minutes = 30;
 int hours = 15;
-
+void setup() 
+{
+  Serial.begin(9600);
+  //Obtiene el número de elementos de un arreglo
+  Serial.println(sizeof(ledPinsSec));
+  for(int i = 0; i< secSize;i++)
+  {
+    pinMode(ledPinsSec[i], OUTPUT);
+  }
+  for(int i = 0; i< minSize;i++)
+  {
+    pinMode(ledPinsMin[i], OUTPUT);
+  }
+  for(int i = 0; i< hourSize;i++)
+  {
+    pinMode(ledPinsHr[i], OUTPUT);
+  }
+}
 void loop() 
 {
     seconds++;
@@ -25,8 +42,30 @@ void loop()
         }
       }
     }
+    //Serial.println((char) seconds);
+    Serial.print("Hours: ");
+    Serial.print(hours);
+    Serial.print(" Minutes: ");
+    Serial.print(minutes);
+    Serial.print(" Seconds: ");
+    Serial.println(seconds);
+    DisplaySeconds();
+    DisplayMinutes();
+    DisplayHours();
+    delay(1000);
+/*
+    DisplaySeconds();
+    DisplayMinutes();
+    DisplayHours();*/
 }
-
+void DisplaySeconds()
+{     
+  for(int i = secSize - 1; i>= 0; i--)
+  {
+    int currentSecond = bitRead(seconds, i);
+    digitalWrite(ledPinsSec[i], currentSecond); 
+  }  
+}
 void DisplayMinutes()
 {     
   for(int i = minSize - 1; i>= 0; i--)
